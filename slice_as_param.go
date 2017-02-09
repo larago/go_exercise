fd8f8a4b9ff4668a11f6d0618944a7c46ac91e6d
Castor <bingeye@foxmail.com>
2017-02-09 16:46:40 +0800

    slice

---
+++ slice_as_param.go
@@ -0,0 +1,14 @@
+package main
+
+func sum(a []int) int {
+	s := 0
+	for i := 0; i < len(a); i++ {
+		s += a[i]
+	}
+	return s
+}
+
+func main() {
+	var arr = [5]int{0, 1, 2, 3, 4}
+	print(sum(arr[:]))
+}
