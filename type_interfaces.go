72064c87172f0f97ae5993b28be1f02ca04c6182
Castor <bingeye@foxmail.com>
2017-02-27 11:32:49 +0800

    type interface

--- type_interfaces.go
+++ type_interfaces.go
@@ -17,28 +17,24 @@ type Shaper interface {
     Area() float64
 }
 
-func main() {
-    var areaIntf Shaper 
-    sq1 := new(Square)
-    sq1.side = 5
-
-    areaIntf = sq1 
-    if t, ok := areaIntf.(*Square); ok {
-        fmt.Printf("The type of areaIntf is: %T\n", t)
-        fmt.Println(t)
-    }
-    if u, ok := areaIntf.(*Circle); ok {
-        fmt.Printf("The type of areaIntf is: %T\n", u)
-        fmt.Println(u)
-    } else {
-        fmt.Println("areaIntf does not contain a variable of type Circle")
-    }
-}
-
 func (sq *Square) Area() float64 {
-    return sq.side * sq.side
+    return sq.side * sq.side 
 }
 
 func (ci *Circle) Area() float64 {
     return ci.radius * ci.radius * math.Pi
+}
+
+func main() {
+    var areaIntf Shaper 
+    sq1 := new(Square)
+    sq1.side = 5
+    areaIntf = sq1
+
+    if t, ok := areaIntf.(*Square); ok {
+        fmt.Printf("The type of areaIntf is: %T\n", t)
+    }
+    if u, ok := areaIntf.(*Circle); ok {
+        fmt.Printf("The type of areaIntf is: %T\n", u)
+    }
 }
