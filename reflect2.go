1888d22751c7c58aa46d59e5c0de796a81cb6f8e
Castor <bingeye@foxmail.com>
2017-02-28 11:01:26 +0800

    usage of reflect

---
+++ reflect2.go
@@ -0,0 +1,21 @@
+package main 
+
+import (
+    "fmt"
+    "reflect"
+)
+
+func main() {
+    var x float64 = 3.4
+    v := reflect.ValueOf(x)
+    fmt.Println("settability of v:", v.CanSet())
+    v = reflect.ValueOf(&x)
+    fmt.Println("type of v:",v.Type())
+    fmt.Println("settability of v:", v.CanSet())
+    v = v.Elem()
+    fmt.Println("The Elem of v is:", v)
+    fmt.Println("settablity of v:", v.CanSet())
+    v.SetFloat(3.1415)
+    fmt.Println(v.Interface())
+    fmt.Println(v)
+}
